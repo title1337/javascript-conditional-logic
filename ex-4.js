7e19de1b502bf807c2164d9a4e61416a6f5eadea
Tle <title1337@gmail.com>
2026-03-19 09:50:22 +0700

    Feat: EX4

--- ex-4.js
+++ ex-4.js
@@ -1,5 +1,14 @@
 //Exercise 4
 // Switch Statement
 let lightBulbStatus = "On";
+switch (lightBulbStatus) {
+    case "On":
+        console.log("The light bulb is on.");
+        break; 
+    case "Off":
+        console.log("The light bulb is off.");
+        break;
+    default:
+        console.log("Please choose the correct input (On/Off/Broken)");
+}
 
-//Start coding here
